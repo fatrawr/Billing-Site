576b8e950acdcbb964a3f972868aa1d44240cc8f
fatrawr <f3ima.nd@gmail.com>
2026-08-10 13:02:45 +0500

    Update routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, useLocation  } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation, RequireAuth  } from "react-router-dom";
 import AppShell from "./components/AppShell.jsx";
 import Welcome from "./pages/Welcome.jsx";
 import SignUp from "./pages/SignUp.jsx";
